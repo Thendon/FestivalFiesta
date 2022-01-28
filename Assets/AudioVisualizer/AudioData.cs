921b8394e6aefb76c2aa052381ce59fdb5becf8d
Florian Herrmann <fherrmann@vreedback.de>
2022-01-28 23:06:21 +0100

    brrrrrr i u i u brrrr

--- Assets/AudioVisualizer/AudioData.cs
+++ Assets/AudioVisualizer/AudioData.cs
@@ -1,48 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class AudioData : MonoBehaviour
 {
-    [SerializeField]
-    AudioSource audioSource = null;
+    //[SerializeField]
+    //AudioSource audioSource = null;
     [SerializeField]
     public int sampleCountPower = 9;
+    [SerializeField]
+    public int freqBandCount = 8;
 
     [HideInInspector]
     public float[] samples;
     [HideInInspector]
+    public float[] freqBands;
+    [HideInInspector]
     public int sampleCount;
+    
+    FMOD.DSP fft;
 
     private void Awake()
     {
-        if (audioSource != null)
-            audioSource = GetComponent<AudioSource>();
+        //if (audioSource != null)
+        //    audioSource = GetComponent<AudioSource>();
 
         //waaaas florian beherrscht bitshifting magic? ?? ? 
         sampleCount = 1 << sampleCountPower;
         sampleCount = Mathf.Clamp(64, sampleCount, 8192);
         samples = new float[sampleCount];
+
+        freqBands = new float[freqBandCount];
+    }
+
+    private void Start()
+    {
+        FMODUnity.RuntimeManager.CoreSystem.createDSPByType(FMOD.DSP_TYPE.FFT, out fft);
+        fft.setParameterInt((int)FMOD.DSP_FFT.WINDOWTYPE, (int)FMOD.DSP_FFT_WINDOW.BLACKMANHARRIS);
+        fft.setParameterInt((int)FMOD.DSP_FFT.WINDOWSIZE, sampleCount * 2);
+
+        FMOD.ChannelGroup channelGroup;
+        FMODUnity.RuntimeManager.CoreSystem.getMasterChannelGroup(out channelGroup);
+        channelGroup.addDSP(FMOD.CHANNELCONTROL_DSP_INDEX.HEAD, fft);
     }
 
     void Update()
     {
-        if (audioSource.clip == null)
-            return;
+        //if (audioSource.clip == null)
+        //    return;
 
-        audioSource.GetSpectrumData(samples, 0, FFTWindow.Rectangular);
-
+        //audioSource.GetSpectrumData(samples, 0, FFTWindow.Rectangular);
+        GetSpectrumData();
+        GenerateFrequenzyBands();
     }
 
-    void MakeFrequenzyBands()
+    void GetSpectrumData()
+    {
+        IntPtr unmanagedData;
+        uint length;
+        fft.getParameterData((int)FMOD.DSP_FFT.SPECTRUMDATA, out unmanagedData, out length);
+        FMOD.DSP_PARAMETER_FFT fftData = (FMOD.DSP_PARAMETER_FFT)Marshal.PtrToStructure(unmanagedData, typeof(FMOD.DSP_PARAMETER_FFT));
+        var spectrum = fftData.spectrum;
+
+        if (fftData.numchannels > 0)
+            for (int i = 0; i < sampleCount; ++i)
+                samples[i] = spectrum[0][i];
+    }
+
+    void GenerateFrequenzyBands()
     {
         int count = 0;
 
-        for (int i = 0; i < 8; i++)
+        int rest = sampleCount % freqBandCount;
+        for (int i = 0; i < freqBandCount; i++)
         {
             float sum = 0;
+            //only works for 512 lol
             int sampleCount = (int)Mathf.Pow(2, i) * 2;
-            if(i == 7)
+            if(i == freqBandCount - 1)
             {
                 sampleCount += 2;
             }
@@ -52,18 +89,7 @@ public class AudioData : MonoBehaviour
                 count++;
             }
 
-            freqBand[i] = sum / count;
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        for (int i = 1; i < samples.Length - 1; i++)
-        {
-            Debug.DrawLine(new Vector3(i - 1, samples[i] + 10, 0), new Vector3(i, samples[i + 1] + 10, 0), Color.red);
-            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(samples[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(samples[i]) + 10, 2), Color.cyan);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), samples[i - 1] - 10, 1), new Vector3(Mathf.Log(i), samples[i] - 10, 1), Color.green);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(samples[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(samples[i]), 3), Color.blue);
+            freqBands[i] = sum / count;
         }
     }
 }
--- Assets/AudioVisualizer/AudioVisualizer.cs
+++ Assets/AudioVisualizer/AudioVisualizer.cs
@@ -7,39 +7,75 @@ public class AudioVisualizer : MonoBehaviour
     [SerializeField]
     AudioData audioData = null;
     [SerializeField]
-    GameObject prefab = null;
+    GameObject samplePrefab = null;
+    [SerializeField]
+    GameObject freqBandPrefab = null;
     [SerializeField]
     float maxHeight = 10.0f;
     [SerializeField]
-    float distance = 100.0f;
+    float sampleDistance = 100.0f;
+    [SerializeField]
+    float freqBandDistance = 5.0f;
 
-    GameObject[] instances = null;
+    GameObject[] sampleInstances = null;
+    GameObject[] freqBandInstances = null;
 
     private void Start()
     {
-        instances = new GameObject[audioData.sampleCount];
+        sampleInstances = new GameObject[audioData.sampleCount];
         float circleFactor = (1.0f / audioData.sampleCount) * Mathf.PI * 2.0f;
         for (int i = 0; i < audioData.sampleCount; i++)
         {
-            GameObject instance = Instantiate(prefab, transform);
-            instance.name = "instance_" + i;
+            GameObject instance = Instantiate(samplePrefab, transform);
+            instance.name = "sample_" + i;
             float pos = i * circleFactor;
+
             Vector3 dir = new Vector3(Mathf.Cos(pos), 0, Mathf.Sin(pos));
-            instance.transform.position = transform.position + dir * distance;
+            instance.transform.position = transform.position + dir * sampleDistance;
             instance.transform.rotation = Quaternion.LookRotation(-dir, Vector3.up);
 
-            instances[i] = instance;
+            sampleInstances[i] = instance;
         }
+
+        freqBandInstances = new GameObject[audioData.freqBandCount];
+        for (int i = 0; i < audioData.freqBandCount; i++)
+        {
+            GameObject instance = Instantiate(freqBandPrefab, transform);
+            instance.name = "freqBand_" + i;
+
+            Vector3 pos = transform.position;
+            pos.x += i * freqBandDistance - audioData.freqBandCount * freqBandDistance * 0.5f;
+            instance.transform.position = pos;
+
+            freqBandInstances[i] = instance;
+        }
+    }
+
+    float lin2dB(float linear)
+    {
+        return Mathf.Clamp(Mathf.Log10(linear) * 20.0f, -80.0f, 0.0f);
     }
 
     private void Update()
     {
         for (int i = 0; i < audioData.sampleCount; i++)
         {
-            GameObject instance = instances[i];
+            GameObject instance = sampleInstances[i];
             Vector3 scale = instance.transform.localScale;
-            scale.y = audioData.samples[i] * maxHeight;
+            scale.y = lin2dB(audioData.samples[i]) * maxHeight;
             instance.transform.localScale = scale;
+        }
+
+        for (int i = 0; i < audioData.freqBandCount; i++)
+        {
+            GameObject instance = freqBandInstances[i];
+            Vector3 scale = instance.transform.localScale;
+            scale.y = audioData.freqBands[i] * maxHeight;
+            instance.transform.localScale = scale;
+
+            Vector3 pos = instance.transform.localPosition;
+            pos.y = scale.y * 0.5f;
+            instance.transform.localPosition = pos;
         }
     }
 }

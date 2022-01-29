0f81c2383cc24dfb40e9744e0afe688b0c8d784d
Melvanus <33779897+Melvanus@users.noreply.github.com>
2022-01-29 13:41:08 +0100

    damage kram

---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour
+{
+    public float Health;
+    private float currentHealth;
+
+    public Action onDeath;
+
+    private void Awake()
+    {
+        currentHealth = Health;
+    }
+
+    public void GetDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        onDeath?.Invoke();
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/DamageDealComponent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealComponent : MonoBehaviour
+{
+    public float baseDamage;
+    public float damageMultiplier;
+
+    public GameObject toDestroyOnCollision;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Damagable d = other.GetComponent<Damagable>();
+        if (d == null)
+            other.GetComponentInParent<Damagable>();
+        if (d == null)
+            other.GetComponentInChildren<Damagable>();
+
+        if (d != null)
+        {
+            d.GetDamage(baseDamage * damageMultiplier);
+            Destroy(toDestroyOnCollision);
+        }
+    }
+
+}

5eff08f7455afb4291b070005df69d473d3b3edf
Florian Herrmann <florianherrmann@web.de>
2022-01-30 12:39:06 +0100

    music manager

--- Assets/Scripts/SimpleProjectile.cs
+++ Assets/Scripts/SimpleProjectile.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class SimpleProjectile : MonoBehaviour
 {
     public float speed = 3.0f;
+    public float liveTime = 10.0f;
+
+    float death;
+
+    private void Awake()
+    {
+        death = liveTime;
+    }
 
     void Update()
     {
         transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+
+        death -= Time.deltaTime;
+        if (death < 0)
+            Destroy(gameObject);
     }
 }

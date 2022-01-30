34d84e4425280f7da8b6859794c1816875da0899
Florian Herrmann <florianherrmann@web.de>
2022-01-30 16:13:20 +0100

    trigger

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -60,7 +60,10 @@ public class SceneLoader : MonoBehaviour
         {
             genrePalette.Add(palette.genre, palette);
         }
+    }
 
+    private void Start()
+    {
         FindObjectOfType<MusicManager>().StartMusic();
     }
 

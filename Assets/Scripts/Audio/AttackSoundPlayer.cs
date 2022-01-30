d524b09c6977b27a070dc01e951e5fcf8c15ccc1
Melvanus <33779897+Melvanus@users.noreply.github.com>
2022-01-30 12:54:17 +0100

    attack sounds

--- Assets/Scripts/Audio/AttackSoundPlayer.cs
+++ Assets/Scripts/Audio/AttackSoundPlayer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AttackSoundPlayer : MonoBehaviour
 {
-    public FMODUnity.EventReference fmodEvent;
 
     string attackBase = "event:/Attack/";
     string eventString;
